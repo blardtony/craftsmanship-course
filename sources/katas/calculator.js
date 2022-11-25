71435ed974fea19abb01bacb7539d3ebfaf4e6c4
Tony Blard <tonyblard55@gmail.com>
2022-11-25 12:38:59 +0100

    Tdd Calculator

--- sources/katas/calculator.js
+++ sources/katas/calculator.js
@@ -1,6 +1,34 @@
 
-function add(){
+/**
+ * 
+ * @param {string} numbers 
+ * @returns {numbers}
+ */
+function add(numbers){
+    if (!numbers) {
+        return 0;
+    }
 
+    let stringPatten = "[,\n]";
+
+    if (numbers.startsWith("//")) {
+        let sub = numbers.substring(2, numbers.indexOf('\n'));
+        stringPatten = "[" + sub + "]"
+        numbers = numbers.substring(numbers.indexOf('\n'))
+    }
+
+    const regexPattern = new RegExp(stringPatten, 'g');
+
+    let numbersArray = numbers.split(regexPattern).filter(value => value).map(value => parseInt(value));
+    
+    let negativeArray = numbersArray.filter(value => value < 0);
+
+    if (negativeArray.length) {
+        let errorMessage = "negative numbers not supported: " + negativeArray.join(" ");
+        throw new Error(errorMessage);
+    }
+    
+    return numbersArray.reduce((a, b) => a + b, 0);
 }
 
 module.exports = { add };
--- sources/katas/calculator.test.js
+++ sources/katas/calculator.test.js
@@ -8,6 +8,55 @@ describe('string calculator', function(){
             expect(add).toBeInstanceOf(Function);
         });
 
+        it('should return 3 when input is "1,2"', function() {
+            const input  = "1,2";
+            let result = add(input);
+            expect(result).toBe(3);
+        });
+
+        it('should return 4 when input is "1,3"', function() {
+            const input  = "1,3";
+            let result = add(input);
+            expect(result).toBe(4);
+        });
+        
+        it('should return 0 when input is ""', function() {
+            const input  = "";
+            let result = add(input);
+            expect(result).toBe(0);
+        }); 
+
+        it('should return 11 when input is "1,2,3,5"', function() {
+            const input  = "1,2,3,5";
+            let result = add(input);
+            expect(result).toBe(11);
+        });
+
+        it('should return 11 when input is "1\n2"', function() {
+            const input  = "1\n2";
+            let result = add(input);
+            expect(result).toBe(3);
+        });
+
+        it('should return 3 when input is "//:)\n1:)2"', function() {
+            const input  = "//:)\n1:)2";
+            let result = add(input);
+            expect(result).toBe(3);
+        });
+
+        it('should return exception negative numbers not supported: -1 when input is "-1,2"', function() {
+            const input  = "-1,2";
+            expect(()=> add(input)).toThrow("negative numbers not supported: -1");
+            // expect(result).toThrow("negative numbers not supported: -1");
+        });
+        
+        it('should return exception negative numbers not supported: -1 -2 when input is "-1, -2 ,2"', function() {
+            const input  = "-1,-2,2";
+            expect(()=> add(input)).toThrow("negative numbers not supported: -1 -2");
+            // expect(result).toThrow("negative numbers not supported: -1");
+        });
+        
+
     });
 
 
